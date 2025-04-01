4fa2ddaf7637cf4623835985653c65180e2563d5
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-04-01 14:25:12 +0000

    Minor Changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import HomeHeader from './Header'
 import HomeFooter from './Footer'
+import ProductList from '../ProductList'
 
 export default function Home() {
     return (
         <div className='flex flex-col'>
             <HomeHeader />
             <HomeFooter />
+            <ProductList />
         </div>
     )
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import { useState, useEffect } from 'react';
+import Image from 'next/image'
 
 interface Product {
   id: number;
+  image: string;
   title: string;
   price: number;
 }
@@ -20,13 +24,15 @@ const ProductList: React.FC = () => {
   }, []);
 
   return (
-    <ul>
+    <div className='flex flex-wrap max-w-[100vw] w-[100vw] justify-center gap-4'>
       {products.map((product) => (
-        <li key={product.id}>
-          {product.title} - ${product.price}
-        </li>
+        <div key={product.id} className='w-[20vw] flex flex-col gap-2 justify-center'>
+          <img src={product.image} alt='Logo' width={100} />
+          <h3 className='text-wrap'>{product.title}</h3>
+          <p>${product.price}</p>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
 
