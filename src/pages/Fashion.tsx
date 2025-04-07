b02ad9c9531576ce9e037400c602b9f9481cd8bb
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-04-07 11:56:01 +0000

    Included A Component

--- src/pages/Fashion.tsx
+++ src/pages/Fashion.tsx
@@ -1,6 +1,7 @@
 import HomeHeader from '../components/Home/Header'
 import HomeHero from '../components/Home/Hero'
 import HomeCategories from '../components/Home/Categories'
+import HomeDiscounts from '../components/Home/Discounts'
 import HomeFooter from '../components/Home/Footer'
 import ProductList from '../components/ProductList'
 
@@ -10,6 +11,7 @@ export default function Home() {
             <HomeHeader />
             <HomeHero />
             <HomeCategories />
+            <HomeDiscounts />
             <HomeFooter />
             <ProductList />
         </div>
