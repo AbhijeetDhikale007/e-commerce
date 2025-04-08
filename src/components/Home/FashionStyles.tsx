621826c18d092b4667a536be618b1537934bfb6f
abhijeetdhikale007 <abhijeetdhikale007@gmail.com>
2025-04-08 06:07:01 +0000

    Created: Fashions Styles

--- src/components/Home/FashionStyles.tsx
+++ src/components/Home/FashionStyles.tsx
@@ -2,19 +2,16 @@
 import Ico from '../../lib/Ico'
 import './CSS/FashionStyles.scss'
 
-function DiscountsContainer({ Text, Title, Message }: { Text: string, Title: string, Message: string }) {
+function FashionStylesContainer({ Text, Title, Prize }: { Text: string, Title: string, Prize: string }) {
     return (
-        <div className='FashionStylesContainer flex flex-col gap-3 justify-center h-[34vh] w-full'>
-            <p className='text-xs'>{Text}</p>
-            <h3 className='text-lg font-semibold'>{Title}</h3>
-            <span className='text-xs opacity-50'>{Message}</span>
-            <div>
-                <button className='flex gap-2 items-center'>
-                    <div className='h-5 w-5'>
-                        <Ico name='PlusButton' />
-                    </div>
-                    <span className='text-xs'>Add to Cart</span>
-                </button>
+        <div className='FashionStylesContainer flex flex-col items-center text-xs min-h-[34vh] min-w-[12vw]'>
+            <div className='ImageDiv flex items-center justify-center bg-white w-full h-[94%] overflow-hidden'>
+                <img className='w-full' src='/vercel.svg' alt={Text} />
+            </div>
+            <div className='TextContainer w-full flex flex-col gap-2'>
+                <p className='text-sm font-semibold'>{Text}</p>
+                <h3>{Title}</h3>
+                <h3>{Prize}</h3>
             </div>
         </div>
     )
@@ -22,8 +19,32 @@ function DiscountsContainer({ Text, Title, Message }: { Text: string, Title: str
 
 export default function HomeFashionStyles() {
     return (
-        <div>
-
+        <div className='HomeFashionStyles py-[2vh] w-screen min-h-[66vh] flex justify-center items-center'>
+            <section className='flex flex-col gap-4 items-center justify-center w-[75%]'>
+                <div className='flex items-center justify-center w-full'>
+                    <div className='flex items-center justify-between w-full'>
+                        <div className='flex flex-col items-start gap-2'>
+                            <h3 className='font-bold text-2xl'>GET YOUR FASHION STYLE</h3>
+                            <p className='opacity-50 text-xs'>28 categories belonging to a total of 15,382 products</p>
+                        </div>
+                        <div className='Buttons flex gap-2'>
+                            <div>
+                                <Ico name='LeftButton' />
+                            </div>
+                            <div>
+                                <Ico name='RightButton' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr className='w-full' />
+                <div className='flex gap-4'>
+                    <FashionStylesContainer Text='LEVIS COTTON' Title='Monica Diara Party Dress' Prize='$893.00' />
+                    <FashionStylesContainer Text='LEVIS COTTON' Title='Onima Black Flower Sandal' Prize='$893.00' />
+                    <FashionStylesContainer Text='LEVIS COTTON' Title='Poncho Sweater International' Prize='$893.00' />
+                    <FashionStylesContainer Text='LEVIS COTTON' Title='Dvalo Office Cotton Suite' Prize='$893.00' />
+                </div>
+            </section>
         </div>
     )
 }
